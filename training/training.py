1d5c59b5f06ecd846c578cbe4fa0773da501b8bd
Venkata Jagannath <venky_jagannath@hotmail.com>
2024-09-04 08:24:22 -0500

    Added lots of images from kaggle dataset

--- training/training.py
+++ training/training.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")
 # results = model.train(data="coco_dataset.yaml", epochs=5)
 
-results = model.train(data="fire_dataset.yaml", epochs=50)
+results = model.train(data="fire_dataset.yaml", epochs=1, workers=1, batch=2)
