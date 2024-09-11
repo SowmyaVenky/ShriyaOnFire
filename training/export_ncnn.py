d9d4d246d0d5566e4f833c485d87dc7714aec044
Venkata Jagannath <venky_jagannath@hotmail.com>
2024-09-11 09:27:27 -0500

    Changed variables

---
+++ training/export_ncnn.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+import os
+import cv2
+
+ROOT_PATH = '/home/sowmyavenky/ShriyaOnFire/training/runs/detect/train/weights/'
+
+BEST_PT = os.path.join(ROOT_PATH, 'best.pt')
+
+# Load the YOLOv8 model
+model = YOLO(BEST_PT)
+
+# Export the model to NCNN format
+model.export(format="ncnn")  
+
+imgdir = os.path.join('../', 'image_extractions', 'roboflowdata', 'train', 'images', '0e25edb4-5c37-40d8-80fc-cb1e4a64408c_frame1750_jpg.rf.3e67bc85c5fcebcf9daa7c9b4f723fd5.jpg')
+
+# Load the exported TFLite model
+TFLITE_PATH = os.path.join(ROOT_PATH, 'best_ncnn_model/')
+tflite_model = YOLO(TFLITE_PATH)
+
+# from ndarray
+im2 = cv2.imread(imgdir)
+results = tflite_model.predict(source=im2) 
+
+# View results
+for r in results:
+    print(r.boxes)  # print the Boxes object containing the detection bounding boxes
---
+++ training/video_testing_ncnn.py
@@ -0,0 +1,62 @@
+import cv2
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator
+import os
+
+ROOT_PATH = '/home/sowmyavenky/ShriyaOnFire/training/runs/detect/train/weights/'
+VIDEO_PATH = '/home/sowmyavenky/ShriyaOnFire/testing_videos/'
+
+BEST_PT = os.path.join(ROOT_PATH, 'best.pt')
+
+# Load the YOLOv8 model
+model = YOLO(BEST_PT)
+
+## Webpage for Raspberry pi 5 
+# https://docs.ultralytics.com/guides/raspberry-pi/#how-do-i-set-up-ultralytics-yolov8-on-a-raspberry-pi-without-using-docker
+
+cap = cv2.VideoCapture(os.path.join(VIDEO_PATH, 'fire_testing10.mp4'))
+# cap = cv2.VideoCapture('C:\\Venky\\Shriya_on_Fire\\ShriyaOnFire\\videos\\homam2.mp4')
+
+# cap = cv2.VideoCapture('C:\\Venky\\Shriya_on_Fire\\ShriyaOnFire\\testing_videos\\fire_testing1.mp4')
+# cap = cv2.VideoCapture('C:\\Venky\\Shriya_on_Fire\\ShriyaOnFire\\testing_videos\\fire_testing2.mp4')
+# cap = cv2.VideoCapture('C:\\Venky\\Shriya_on_Fire\\ShriyaOnFire\\testing_videos\\fire_testing3.mp4')
+
+# cap = cv2.VideoCapture('/home/sowmyavenky/ShriyaOnFire/videos/homam1.mp4')
+
+# cap = cv2.VideoCapture('C:\\Venky\\Shriya_on_Fire\\ShriyaOnFire\\testing_videos\\fire_testing4.mp4')
+# cap = cv2.VideoCapture('C:\\Venky\\Shriya_on_Fire\\ShriyaOnFire\\testing_videos\\fire_testing5.webm')
+# cap = cv2.VideoCapture('C:\\Venky\\Shriya_on_Fire\\ShriyaOnFire\\testing_videos\\fire_testing6.mp4')
+# cap = cv2.VideoCapture('C:\\Venky\\Shriya_on_Fire\\ShriyaOnFire\\testing_videos\\fire_testing7.mp4')
+# cap = cv2.VideoCapture('C:\\Venky\\Shriya_on_Fire\\ShriyaOnFire\\testing_videos\\fire_testing8.mp4')
+# cap = cv2.VideoCapture('C:\\Venky\\Shriya_on_Fire\\ShriyaOnFire\\testing_videos\\fire_testing9.mp4')
+
+cap.set(3, 640)
+cap.set(4, 480)
+
+while cap.isOpened():
+    ret, frame1 = cap.read()
+    
+    if  frame1 is None:
+      break
+        
+    frame = cv2.resize(frame1, (640, 480)) 
+    # cv2.imshow('YOLO', frame)
+    
+    results = model.predict(source=frame) 
+
+    for r in results:        
+      annotator = Annotator(frame)
+        
+      boxes = r.boxes
+      for box in boxes:
+        b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
+        c = box.cls
+        annotator.box_label(b, model.names[int(c)], color=(0, 255, 0),txt_color=(0, 0, 255),)
+          
+    img = annotator.result()  
+    cv2.imshow('YOLO V8 Detection', img)   
+    
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
